193e613df8a2baba02dc9a92d19365da00d72b95
themayurpawar <114336824+themayurpawar@users.noreply.github.com>
2026-06-26 02:03:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const pencil = document.querySelector(".pencil");
+const content = document.querySelector(".content-wrapper");
 
 document.addEventListener("mousemove", (e) => {
 
     const x = (e.clientX - window.innerWidth / 2) / 40;
     const y = (e.clientY - window.innerHeight / 2) / 40;
 
+    // Pencil movement
     pencil.style.transform =
         `translate(${x}px, ${y}px)`;
+
+    // Content movement
+    content.style.transform =
+        `translate(calc(-50% + ${x * 0.25}px), calc(-50% + ${y * 0.25}px))`;
 
 });
